7e31c425a6544886a916e451fb615a84d5df4f24
Jan Amann <jan@amann.work>
2024-10-03 12:00:47 +0200

    test update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import {headers} from 'next/headers';
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 p-4">
+      <h1>Hello world!</h1>
       <pre>
         VERCEL_PROJECT_PRODUCTION_URL:{' '}
         {process.env.VERCEL_PROJECT_PRODUCTION_URL}
