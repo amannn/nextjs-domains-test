7c2e5e4435d91798362c9416263f0b58a22559a3
Jan Amann <jan@amann.work>
2024-10-03 12:04:31 +0200

    remove headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import {headers} from 'next/headers';
-
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 p-4">
@@ -8,7 +6,6 @@ export default function Home() {
         VERCEL_PROJECT_PRODUCTION_URL:{' '}
         {process.env.VERCEL_PROJECT_PRODUCTION_URL}
       </pre>
-      <pre>{JSON.stringify(Array.from(headers().entries()), null, 2)}</pre>
     </div>
   );
 }
